4bd5aace8d598a18a1d5ea947c5205ec5b699eb3
MohammadTasin659 <islamtasin080@gmail.com>
2025-05-24 18:32:10 +0400

    Create middleware.js

---
+++ src/middleware.js
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: "/integrations/:path*",
+};
+
+export function middleware(request) {
+  const requestHeaders = new Headers(request.headers);
+
+  request.nextUrl.href = `https://www.create.xyz/${request.nextUrl.pathname}`;
+
+  return NextResponse.rewrite(request.nextUrl, {
+    request: {
+      headers: requestHeaders,
+    },
+  });
+}
